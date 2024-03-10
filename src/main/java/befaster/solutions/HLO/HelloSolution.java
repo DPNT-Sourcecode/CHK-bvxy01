e51d6a854191d79a4c99096dff32a3cfa823525f
sbx_user1051 <sbx_user1051@169.254.10.81>
2024-03-10 23:31:36 +0000

    Sun Mar 10 23:31:36 UTC 2024

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -2,7 +2,18 @@ package befaster.solutions.HLO;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        return "Hello, World!";
+    	
+    	if (friendName == null || friendName.isEmpty() || friendName.isBlank()) {
+    		throw new IllegalArgumentException("Invalid name");
+    	}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("Hello, ");
+    	sb.append(friendName);
+    	sb.append("!");
+    	
+        return sb.toString();
     }
 }
+
 
