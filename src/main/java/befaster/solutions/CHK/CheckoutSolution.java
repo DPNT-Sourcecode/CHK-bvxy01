f3d4b2066a6e7f7f87de27c9215697e668811d68
sbx_user1051 <sbx_user1051@169.254.10.81>
2024-03-10 23:38:41 +0000

    Sun Mar 10 23:38:41 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,5 +1,8 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         
@@ -8,8 +11,17 @@ public class CheckoutSolution {
     		return 0;
     	}
     	
+    	int total = 0;
+    	
+    	Map<Character, Integer> checkoutItems = new HashMap<>();
+    	
+    	for (char item : skus.toCharArray()) {
+    		checkoutItems.put(item, checkoutItems.getOrDefault(item, 0) + 1);
+    	}
+    	
     	
     	
     }
 }
+
 
