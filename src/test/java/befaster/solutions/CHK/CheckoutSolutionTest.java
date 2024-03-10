12af8f4a380478e3d87a2d730d1cf4bc86ee9914
sbx_user1051 <sbx_user1051@169.254.10.81>
2024-03-10 23:50:41 +0000

    Sun Mar 10 23:50:41 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,37 @@
 package befaster.solutions.CHK;
 
-public class CheckoutSolutionTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CheckoutSolutionTest {
+    private CheckoutSolution checkoutSolution;
+
+    @BeforeEach
+    public void setUp() {
+    	checkoutSolution = new CheckoutSolution();
+    }
+    
+    @Test
+    public void testCheckoutSolution_withTwoAProducts() {
+    	assertThat(checkoutSolution.checkout("AA"), equalTo(100));
+    }
+    
+    @Test
+    public void testCheckoutSolution_withThreeAProducts() {
+    	assertThat(checkoutSolution.checkout("AAA"), equalTo(130));
+    }
+    
+    @Test
+    public void testCheckoutSolution_withTwoBProducts() {
+    	assertThat(checkoutSolution.checkout("BB"), equalTo(45));
+    }
+    
+    @Test
+    public void testCheckoutSolution_withABProducts() {
+    	assertThat(checkoutSolution.checkout("AB"), equalTo(80));
+    }
 }
+
